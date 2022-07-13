4f90f2f6b163a09be99c6008cebed708dfd7383b
zebinfzu <1185261506@qq.com>
2022-07-13 11:11:17 +0800

    react-router-dom是用Route组件渲染

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className='App'>
-      
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product" element={<Product />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function Product() {
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
+}
+
+export default App;
